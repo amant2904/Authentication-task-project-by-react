b0d295c6115b22fda3634c702cf976e1d7c0ec87
amant2904 <tiwariaman2904@gmail.com>
2023-03-25 16:03:20 +0530

    sending first request for signup and getting error

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -14,9 +14,32 @@ const AuthForm = () => {
     setIsLogin((prevState) => !prevState);
   };
 
-  const submit_handler = (e) => {
+  const submit_handler = async (e) => {
     e.preventDefault();
-    setLoading(true);
+    if (email.current.value.includes("@") && password.current.value.trim().length > 0) {
+      setLoading(true);
+      let res = await fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]", {
+        method: 'POST',
+        headers: {
+          'Content-Type': "application/json"
+        },
+        body: {
+          email: email.current.value,
+          password: password.current.value
+        }
+      })
+      let data = await res.json();
+      console.log(data);
+      if (!res.ok) {
+        alert(data.error.message);
+      }
+      setLoading(false);
+    }
+    else {
+      setTimeout(() => {
+        alert("Invalid Input")
+      }, 1000)
+    }
   }
 
   return (
